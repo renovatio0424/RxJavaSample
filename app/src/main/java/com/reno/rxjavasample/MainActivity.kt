a3ef4e23626f57cccd655ec339c8f58e5292cc94
Reno <reno.kim@nexstreaming.com>
2019-08-02 11:49:02 +0900

    Initial commit

---
+++ app/src/main/java/com/reno/rxjavasample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.reno.rxjavasample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
